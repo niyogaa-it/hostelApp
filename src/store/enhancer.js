be2a83735dfb84472a69925d881d9f28b632cd1d
Anumita <anumita.banerjee@intglobal.com>
2026-04-21 15:34:44 +0530

    Clean old files

--- src/store/enhancer.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const Enhancers = composeEnhancers(applyMiddleware(thunk));
